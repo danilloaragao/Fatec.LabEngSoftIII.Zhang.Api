78d5776cbc58db4ace429247c42dfeb6bf1e290e
Danillo Felipe Aragão <danillo2aragao@gmail.com>
2020-09-23 07:10:44 -0300

    Correção do retorna da JogoController

--- Fatec.LabEngSoftIII.Zhang.Api/Controllers/JogoController.cs
+++ Fatec.LabEngSoftIII.Zhang.Api/Controllers/JogoController.cs
@@ -1,5 +1,4 @@
-﻿using Fatec.LabEngSoftIII.Zhang.Api.Entidades;
-using Fatec.LabEngSoftIII.Zhang.Api.Entidades.Entradas;
+﻿using Fatec.LabEngSoftIII.Zhang.Api.Entidades.Entradas;
 using Fatec.LabEngSoftIII.Zhang.Api.Entidades.Saidas;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -51,25 +50,28 @@ namespace Fatec.LabEngSoftIII.Zhang.Api.Controllers
                 Experiencia = 400,
                 ExperienciaProximoNivel = 50,
                 Nivel = 4,
-                Skins = new List<Skin>()
+                Skins = new List<RespSkin>()
                 {
-                    new Skin()
+                    new RespSkin()
                     {
                         Id = 1,
                         Nivel = 1,
-                        Descricao = "Skin 1"
+                        Descricao = "Skin 1",
+                        Ativo = false
                     } ,
-                    new Skin()
+                    new RespSkin()
                     {
                         Id = 2,
                         Nivel = 2,
-                        Descricao = "Skin 2"
+                        Descricao = "Skin 2",
+                        Ativo = true
                     } ,
-                    new Skin()
+                    new RespSkin()
                     {
                         Id = 3,
                         Nivel = 3,
-                        Descricao = "Skin 3"
+                        Descricao = "Skin 3",
+                        Ativo = false
                     }
                 },
                 Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
