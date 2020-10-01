d328f46a5e93743861e57f3ba66abd9aafa307cf
Danillo Felipe Aragão <danillo2aragao@gmail.com>
2020-10-01 06:00:09 -0300

    Ajustes de entradas e saídas

---
+++ Fatec.LabEngSoftIII.Zhang.Api/Entidades/Saidas/RespRanking.cs
@@ -0,0 +1,10 @@
+﻿namespace Fatec.LabEngSoftIII.Zhang.Api.Entidades.Saidas
+{
+    public class RespRanking
+    {
+        public int Posicao { get; set; }
+        public string Login { get; set; }
+        public int Experiencia { get; set; }
+        public int Nivel { get; set; }
+    }
+}
