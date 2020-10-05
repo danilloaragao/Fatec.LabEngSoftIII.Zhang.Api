1f8f842368049a1a79b6b68a073f5b74515be7a4
Danillo Felipe Aragão <danillo2aragao@gmail.com>
2020-10-05 10:23:13 -0300

    Teste CORS

--- Fatec.LabEngSoftIII.Zhang.Api/Startup.cs
+++ Fatec.LabEngSoftIII.Zhang.Api/Startup.cs
@@ -30,15 +30,15 @@ namespace Fatec.LabEngSoftIII.Zhang.Api
                         Description = "API Desenvolvida para o Jogo da Forca Zhang.<br>Trabalho para a matéria de Engenharia de Software III do curso de ADS (noite) da FATEC-SP<br><br>Integrantes:<br>Allan Prado de Oliveira Moura<br>Augusto Albuquerque Reis<br>Bruna Ramos<br>Danillo Felipe Aragão<br>Danilo Tupinambá Polizeli<br>Gabriel Alves da Silva<br>Gustavo Rocha da Silva<br>Shayanne Crispim de Medeiros Amorim<br>Tatiana Rodrigues de Oliveira",
                     });
             });
-            services.AddCors(
-                c => c.AddPolicy("policy",
-                builder =>
-                {
-                    builder.WithOrigins("https://zhang-bd.herokuapp.com", "http://zhang-bd.herokuapp.com", "https://localhost:4200", "http://localhost:4200")
-                           .WithHeaders()
-                           .AllowAnyHeader()
-                           .AllowCredentials();
-                }));
+            //services.AddCors(
+            //    c => c.AddPolicy("policy",
+            //    builder =>
+            //    {
+            //        builder.WithOrigins("https://zhang-bd.herokuapp.com", "http://zhang-bd.herokuapp.com", "https://localhost:4200", "http://localhost:4200")
+            //               .WithHeaders()
+            //               .AllowAnyHeader()
+            //               .AllowCredentials();
+            //    }));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -62,6 +62,9 @@ namespace Fatec.LabEngSoftIII.Zhang.Api
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Jogo da Forca Zhang");
                 c.RoutePrefix = string.Empty;
             });
+
+            app.UseAuthentication();
+            app.UseCors(b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
             //app.UseCors(option => option.AllowAnyOrigin()
             //                            .AllowAnyMethod()
